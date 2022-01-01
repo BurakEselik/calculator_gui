9f3ed23c4a77102cbd5f0d8f02b4b4abe0512a27
BurakEselik <eselik.burak@gmail.com>
2022-01-01 08:15:19 +0300

    modified

--- calc.py
+++ calc.py
@@ -27,6 +27,27 @@ class Calc(QtWidgets.QMainWindow):
         self.ui.btn_multi.clicked.connect(self.edit_screen)
         self.ui.btn_clear.clicked.connect(self.clear_screen)
         self.ui.btn_back.clicked.connect(self.clear_one)
+        self.ui.btn_equal.clicked.connect(self.calculate)
+
+    def calculate(self):
+        text_1 = '<html><head/><body><p align="right">'
+        text_2 = '</p></body></html>'
+        result = str()
+        lbl_screen = self.ui.lbl_screen.text()
+        soup = BeautifulSoup(lbl_screen, 'html.parser')
+
+        if soup.p.get_text() and soup.p.get_text()[-1] not in '+-/x':
+            result = soup.p.get_text().replace('x','*')    
+            try:
+                result = eval(result)
+                if isinstance(result, float):
+                    result = round(result, 6)
+            except ZeroDivisionError:
+                result = 'error'
+            except NameError:
+                result = ''
+            self.ui.lbl_screen.setText(text_1 + str(result) + text_2)
+
 
     def clear_one(self):
         text_1 = '<html><head/><body><p align="right">'
@@ -35,12 +56,25 @@ class Calc(QtWidgets.QMainWindow):
         soup = BeautifulSoup(lbl_screen, 'html.parser')
         self.ui.lbl_screen.setText(text_1 + soup.p.get_text()[:-1] + text_2)
 
+        if len(soup.p.get_text()) <= 11: 
+            font = QtGui.QFont()
+            font.setPointSize(36)
+            self.ui.lbl_screen.setFont(font)
+        elif len(soup.p.get_text()) <= 17:
+            font = QtGui.QFont()
+            font.setPointSize(24)
+            self.ui.lbl_screen.setFont(font)
+        else:
+            pass
+
     def clear_screen(self):
         result = ''
         text_1 = '<html><head/><body><p align="right">'
         text_2 = '</p></body></html>'
         self.ui.lbl_screen.setText(text_1 +  result + text_2)
-
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        self.ui.lbl_screen.setFont(font)
 
     def edit_screen(self):
         sender = self.sender().text()
@@ -51,60 +85,35 @@ class Calc(QtWidgets.QMainWindow):
         soup = BeautifulSoup(lbl_screen, 'html.parser')
         print(sender)
 
-        print('buraya bak: ', soup.p.get_text())
-        if soup.p.get_text() == '0':
-            print('buraya girildi')
-            self.ui.lbl_screen.setText(text_1 + '' + text_2)
+        if len(soup.p.get_text()) == 25:
+            result == soup.p.get_text()
+            self.ui.lbl_screen.setText(text_1 + result + text_2)
+            #TODO add max range on the 2. screen 
+            
+
+        if len(soup.p.get_text()) == 11: 
+            font = QtGui.QFont()
+            font.setPointSize(24)
+            self.ui.lbl_screen.setFont(font)
+        elif len(soup.p.get_text()) >= 17:
+            font = QtGui.QFont()
+            font.setPointSize(16)
+            self.ui.lbl_screen.setFont(font)
+        else:
+            pass
 
         if sender in '/x+-':
             try:
-                if sender == '+':
-                    if soup.p.get_text()[-1] != '+':
-                        result = soup.p.get_text() + '+'
-                    else:
-                        result = soup.p.get_text()
-
-                if sender == '-':
-                    if soup.p.get_text()[-1] != '-':
-                        result = soup.p.get_text() + '-'
-                    else:
-                        result = soup.p.get_text()
-
-                if sender == 'x':
-                    if soup.p.get_text()[-1] != 'x':
-                        result = soup.p.get_text() + 'x'
-                    else:
-                        result = soup.p.get_text()
-
-                if sender == '/':
-                    if soup.p.get_text()[-1] != '/':
-                        result = soup.p.get_text() + '/'
-                    else:
-                        result = soup.p.get_text()
+                last_sign = soup.p.get_text()[-1]
+                if (last_sign != '+') and (last_sign != '-') and (last_sign != '/') and (last_sign != 'x'):
+                    result = soup.p.get_text() + sender
+                else:
+                    result = soup.p.get_text()
             except IndexError as err:
                 pass
 
         elif sender in '0123456789':
-            if sender == '0':
-                result = soup.p.get_text() + '0'
-            elif sender == '1':
-                result = soup.p.get_text() + '1'
-            elif sender == '2':
-                result = soup.p.get_text() + '2'
-            elif sender == '3':
-                result = soup.p.get_text() + '3'
-            elif sender == '4':
-                result = soup.p.get_text() + '4'
-            elif sender == '5':
-                result = soup.p.get_text() + '5'
-            elif sender == '6':
-                result = soup.p.get_text() + '6'
-            elif sender == '7':
-                result = soup.p.get_text() + '7'
-            elif sender == '8':
-                result = soup.p.get_text() + '8'
-            elif sender == '9':
-                result = soup.p.get_text() + '9'
+            result = soup.p.get_text() + sender
 
         self.ui.lbl_screen.setText(text_1 +  result + text_2)
 
